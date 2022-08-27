60c0012a38af9162707d1bdf2e0e92994bbe97c6
pbdiro <104860032+rdcodigo@users.noreply.github.com>
2022-08-27 17:42:11 -0300

    Add Menu Change

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,48 @@
 const descriptions = document.querySelectorAll(".description");
 
-console.log(descriptions)
+const arrayOfDescriptions = [];
+for (var i = 0; i < descriptions.length; i++) {
+    var item = descriptions[i];
+    arrayOfDescriptions.push(item);
+}
 
-descriptions.forEach(
-    function(description){
-        description.addEventListener(
+const menuButtons = document.querySelectorAll(".menuButton");
+
+const arrayOfMenuButtons = [];
+for (var i = 0; i < menuButtons.length; i++) {
+    var item = menuButtons[i];
+    arrayOfMenuButtons.push(item);
+}
+
+const descriptionsId = [ 
+    "home",
+    "about",
+    "skills",
+    "portfolio",
+    "contacts"
+];
+
+arrayOfMenuButtons.forEach(
+    menuButton => {
+        menuButton.addEventListener(
             'click',
-            function(event){
-                const el = event.target
-                console.log(el.id)
+            event=>{
+                let index = arrayOfMenuButtons.indexOf(event.target);
+                let descId = descriptionsId[index];
+                console.log(descId);
+                hidden(descId);
             }
         )
     }
 )
 
-function hidden(e){
-    let id = e.target.id
-    console.log(id)
-    descriptions.forEach(
-        description=>{
-            if(id == description.id){
-                description.style.display = "hidden";
+function hidden(descId){
+    arrayOfDescriptions.forEach(
+        description =>{
+            console.log(description.id);
+            console.log(description);
+            if(descId == description.id){
+                description.style.display = "flex";
             }else{
                 description.style.display = "none";
             }
